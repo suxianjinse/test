7de4a27708754f7473063ef51f68057601bdcd7c
HUI <17706713606@163.com>
2017-07-24 11:38:38 +0800

    Initial commit

---
+++ src/test001/test0001.java
@@ -0,0 +1,16 @@
+package test001;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class test0001 extends Application {
+
+    public static void main(String[] args) {
+        System.out.print("hell world!!!");
+}
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
